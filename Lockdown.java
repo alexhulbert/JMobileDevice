7c43d6cddbd40b998a81e5393ab39bcacacf1fe4
Alex Hulbert <alex@alexhulbert.com>
2014-03-17 15:54:29 -0400

    Started Lockdown constructors Nothing special

--- Lockdown.java
+++ Lockdown.java
@@ -1,9 +1,20 @@
 package com.alexhulbert.jmobiledevice;
 
+import static com.alexhulbert.jmobiledevice.Pymobiledevice.pi;
+
 /**
  *
  * @author Taconut
  */
 public class Lockdown extends Wrapper {
-
+    public Lockdown() {
+        Pymobiledevice.use("pymobiledevice", "lockdown");
+        pi.exec(id + ".lockdown=" + id);
+    }
+    
+    public Lockdown(String id) {
+        Pymobiledevice.use("pymobiledevice", "lockdown");
+        this.id = id;
+        pi.exec(id + ".lockdown=" + id);
+    }
 }
