4ca09832612e5a90886b201330f78ec93d39983e
Taconut <alex@alexhulbert.com>
2014-04-19 18:41:23 -0400

    This has been sitting here for awhile. What did I fix?

--- Lockdown.java
+++ Lockdown.java
@@ -1,14 +1,36 @@
 package com.alexhulbert.jmobiledevice;
 
 import static com.alexhulbert.jmobiledevice.Pymobiledevice.pi;
+import com.alexhulbert.jmobiledevice.diagnostics.Keys;
+import java.util.ArrayList;
+import java.util.List;
+import org.python.core.PyList;
+import org.python.core.PyObject;
 
 /**
  *
  * @author Taconut
  */
 public class Lockdown extends Wrapper {
+    public static String[] listDevices() {
+        List<String> ret = new ArrayList<String>();
+        Pymobiledevice.use("pymobiledevice", "lockdown");
+        PyObject[] raws = new PyList(pi.eval("lockdown.list_devices()")).getArray();
+        for (PyObject i : raws) {
+            ret.add(i.toString());
+        }
+        return ret.toArray(new String[ret.size()]);
+    }
+    
     public Lockdown() {
         Pymobiledevice.use("pymobiledevice", "lockdown");
+        pi.exec(id + "= lockdown.LockdownClient()");
+        pi.exec(id + ".lockdown=" + id);
+    }
+    
+    public Lockdown(char[] UDID) {
+        Pymobiledevice.use("pymobiledevice", "lockdown");
+        pi.exec(id + "= lockdown.LockdownClient('" + new String(UDID) + "')");
         pi.exec(id + ".lockdown=" + id);
     }
     
@@ -16,5 +38,8 @@ public class Lockdown extends Wrapper {
         Pymobiledevice.use("pymobiledevice", "lockdown");
         this.id = id;
         pi.exec(id + ".lockdown=" + id);
+        
     }
+    
+    
 }
