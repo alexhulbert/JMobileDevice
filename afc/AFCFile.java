003ef3bfae9c34ce4c3458706cd5d591cf6a3a77
Taconut <alex@alexhulbert.com>
2014-04-22 13:28:44 -0400

    Finished Constructors for AFCFile (No Commint)

--- afc/AFCFile.java
+++ afc/AFCFile.java
@@ -1,6 +1,7 @@
 package com.alexhulbert.jmobiledevice.afc;
 
 import com.alexhulbert.jmobiledevice.Pymobiledevice;
+import static com.alexhulbert.jmobiledevice.Pymobiledevice.pi;
 import com.alexhulbert.jmobiledevice.Utils;
 
 public class AFCFile {
@@ -9,26 +10,35 @@ public class AFCFile {
     public AFCFile(String file) {
         Pymobiledevice.use("pymobiledevice", "afc");
         id = Utils.unique();
+        pi.exec(id + "=afc.AFCFile('" + file + "')");
     }
     
     public AFCFile(String file, String mode) {
         Pymobiledevice.use("pymobiledevice", "afc");
         id = Utils.unique();
+        pi.exec(id + "=afc.AFCFile('" + file + "','" + mode + "')");
     }
     
     public AFCFile(String file, String mode, AFC parent) {
         Pymobiledevice.use("pymobiledevice", "afc");
         id = Utils.unique();
+        pi.exec(id + "=afc.AFCFile('" + file + "','" + mode + "'," + parent.getId() + ")");
     }
     
     public AFCFile(String file, AFC parent) {
         Pymobiledevice.use("pymobiledevice", "afc");
         id = Utils.unique();
+        //Should it really be 'r' by default
+        pi.exec(id + "=afc.AFCFile('" + file + "','r'," + parent.getId() + ")"); //TODO: Consult PythEch on this
     }
     
-    public AFCFile(char[] id) {
+    private AFCFile(String id, boolean dummy) {
         Pymobiledevice.use("pymobiledevice", "afc");
-        this.id = id.toString();
+        this.id = id;
+    }
+    
+    public static AFCFile connect(String id) {
+        return new AFCFile(id, false);
     }
     
     public String getId() {
